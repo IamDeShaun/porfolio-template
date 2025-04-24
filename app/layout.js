f06a15f9d6b7179a5278460d75fd30147161b05f
DeShaun Jones <IamDeShaun@users.noreply.github.com>
2025-04-24 11:00:21 -0400

    Font Update
    Updating root layout. Adding new google fonts.

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const Outfit = Outfit({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const Ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
 });
 
 export const metadata = {
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${Outfit.className} ${Ovo.className} antialiased`}
       >
         {children}
       </body>

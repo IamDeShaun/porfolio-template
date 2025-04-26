dd059470e4e373f49621806efab068019a93299b
DeShaun Jones <IamDeShaun@users.noreply.github.com>
2025-04-25 22:51:50 -0400

    Update Contact.jsx
    Updated contact component

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -8,17 +8,9 @@ const Contact = () => {
         <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
     <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
     <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi laudantium in odio illum debitis ducimus facilis nulla corrupti vel, eum soluta similique sunt, cum autem atque sequi harum cumque? Unde.</p>
+    <h3>ADD WEB FORM HERE</h3>
 
-    <form className='max-w-2xl mx-auto'>
-        <div className='grid sm:auto-cols-[minmax(200px,1fr)] gap-6 mt-10 mb-8'>
-            <input type="text" placeholder='Enter your name' required 
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
-            <input type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
-        </div>
-        <textarea row='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
-
-        <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit now <Image src={assets.right_arrow_white} alt="Submit Button" className='w-4' /></button>
-    </form>
+    
     </div>
   )
 }

5eab9512cf6c1d364582f7d7ed01d7aab26e4128
DeShaun Jones <IamDeShaun@users.noreply.github.com>
2025-04-26 16:27:26 -0400

    Footer.jsx
    Created footer component

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
     <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
     <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi laudantium in odio illum debitis ducimus facilis nulla corrupti vel, eum soluta similique sunt, cum autem atque sequi harum cumque? Unde.</p>
-    <h3>ADD WEB FORM HERE</h3>
+    <h3 className="text-center">ADD WEB FORM HERE</h3>
 
     
     </div>
---
+++ app/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2' />
+
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

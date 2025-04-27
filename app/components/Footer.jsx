e520dbfa925d7121440e33305cb3326fd3d78764
DeShaun Jones <IamDeShaun@users.noreply.github.com>
2025-04-26 22:39:00 -0400

    Footer.jsx
    Added footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,9 +8,18 @@ const Footer = () => {
         <div className='text-center'>
             <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2' />
 
-            <div>
-                
+            <div className='w-max flex items-center gap-2 mx-auto'>
+            <Image src={assets.mail_icon} alt='' className='w-6' />
+                addemailhere@gmail.com 
             </div>
+        </div>
+        <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+            <p>© 2025 ADD SITE BRAND HERE. All rights reserved.</p>
+            <ul className='flex items-center gap-10'>
+                <li><a target='_blank' href="#">GitHub</a></li>
+                <li><a target='_blank' href="#">LinkedIn</a></li>
+
+            </ul>
         </div>
     </div>
   )

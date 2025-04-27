3e90f04f1e872cc7e16e56be3e11b58e6c1b6c4c
DeShaun Jones <IamDeShaun@users.noreply.github.com>
2025-04-27 13:56:07 -0400

    Updated Header
    Updated Header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 
-const header = () => {
+const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
             <div>
@@ -21,4 +21,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
